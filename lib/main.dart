801c5d8ebe32c863afe4bd66dec3b422ba57d791
Roch Bajracharya <rochbajracharya@Rochs-MacBook-Pro.local>
2023-11-10 17:03:54 -0500

    adding dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,43 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
 var kColorScheme =
-    ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 255, 92, 236));
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 255, 92, 236));
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  seedColor: Colors.black,
+  brightness: Brightness.dark,
+);
 
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kDarkColorScheme.onPrimaryContainer,
+          foregroundColor: kDarkColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: ThemeData().textTheme.titleLarge?.copyWith(
+                    fontWeight: FontWeight.bold,
+                    color: kDarkColorScheme.onSecondaryContainer,
+                    fontSize: 16,
+                  ),
+            ),
+      ),
       theme: ThemeData().copyWith(
         useMaterial3: true,
         colorScheme: kColorScheme,
